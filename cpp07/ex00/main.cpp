272c36098969cf9f794a6131315a063beaea765a
Jean-antoine Da silva <jeada-si@made-f0Ar2s7.clusters.42paris.fr>
2024-09-20 13:45:17 +0200

    cpp07 fix

--- cpp07/ex00/main.cpp
+++ cpp07/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 13:11:45 by jeada-si          #+#    #+#             */
-/*   Updated: 2024/09/19 11:54:20 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/09/20 13:41:26 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,53 @@
 #include <iomanip>
 #include <iostream>
 
-int	main( void ) {
-	int a = 2;
-	int b = 3;	
-	::swap( a, b );
-	std::cout << "a = " << a << ", b = " << b << std::endl;
-	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
-	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
-	std::string c = "chaine1";
-	std::string d = "chaine2";
-	::swap(c, d);
-	std::cout << "c = " << c << ", d = " << d << std::endl;
-	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
-	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
-	double e = 4.0;
-	double f = 4.1;
-	::swap(e, f);
-	std::cout << "e = " << e << ", f = " << f << std::endl;
-	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
-	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
-	return 0;
+// int	main( void ) {
+// 	int a = 2;
+// 	int b = 3;	
+// 	::swap( a, b );
+// 	std::cout << "a = " << a << ", b = " << b << std::endl;
+// 	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+// 	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+// 	std::string c = "chaine1";
+// 	std::string d = "chaine2";
+// 	::swap(c, d);
+// 	std::cout << "c = " << c << ", d = " << d << std::endl;
+// 	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+// 	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+// 	double e = 4.0;
+// 	double f = 4.1;
+// 	::swap(e, f);
+// 	std::cout << "e = " << e << ", f = " << f << std::endl;
+// 	std::cout << "min( e, f ) = " << ::min( e, f ) << std::endl;
+// 	std::cout << "max( e, f ) = " << ::max( e, f ) << std::endl;
+// 	return 0;
+// }
+
+class Awesome
+{
+  public:
+    Awesome(void) : _n(0) {}
+    Awesome( int n ) : _n( n ) {}
+    Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+    int get_n() const { return _n; }
+  private:
+    int _n;
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
+int main(void)
+{
+	Awesome a(2), b(4);
+
+        swap(a, b);
+        std::cout << a << " " << b << std::endl;
+        std::cout << max(a, b) << std::endl;
+        std::cout << min(a, b) << std::endl;
+        return (0);
 }
--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/18 15:22:17 by jeada-si          #+#    #+#             */
-/*   Updated: 2024/09/18 15:37:18 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/09/20 13:43:58 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,53 @@
 #include <iostream>
 #include <iomanip>
 
-void	ft_string_print(std::string s)
+// void	ft_string_print(std::string& s)
+// {
+// 	std::cout << "string is " << s << std::endl;
+// }
+
+// void	ft_int_print(int& i)
+// {
+// 	std::cout << "int is " << i << std::endl;
+// }
+
+// int	main(void)
+// {
+// 	std::string	array_s[4] = {"test1", "test2", "test3", "test4"};
+// 	int			array_i[4] = {1, 2, 3, 4};
+
+// 	iter(array_s, 4, ft_string_print);
+// 	iter(array_i, 4, ft_int_print);
+// }
+
+class Awesome
 {
-	std::cout << "string is " << s << std::endl;
+  public:
+    Awesome( void ) : _n( 42 ) { return; }
+    int get( void ) const { return this->_n; }
+  private:
+    int _n;
+};
+
+std::ostream & operator<<( std::ostream & o, Awesome const & rhs )
+{
+  o << rhs.get();
+  return o;
 }
 
-void	ft_int_print(int i)
+template< typename T >
+void print( T& x )
 {
-	std::cout << "int is " << i << std::endl;
+  std::cout << x << std::endl;
+  return;
 }
 
-int	main(void)
-{
-	std::string	array_s[4] = {"test1", "test2", "test3", "test4"};
-	int			array_i[4] = {1, 2, 3, 4};
+int main() {
+  int tab[] = { 0, 1, 2, 3, 4 };
+  Awesome tab2[5];
 
-	iter(array_s, 4, ft_string_print);
-	iter(array_i, 4, ft_int_print);
+  iter( tab, 5, print<int> );
+  iter( tab2, 5, print<Awesome> );
+
+  return 0;
 }
