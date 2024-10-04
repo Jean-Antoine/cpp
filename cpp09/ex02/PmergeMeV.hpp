a9ee0e0e2d4f8a029165805de968516fa7f8c251
Jean-antoine Da silva <jeada-si@made-f0Ar2s7.clusters.42paris.fr>
2024-10-04 10:05:47 +0200

    push before list algo

--- cpp09/ex02/PmergeMeV.hpp
+++ cpp09/ex02/PmergeMeV.hpp
@@ -1,35 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*   PmergeMeV.hpp                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 08:59:12 by jeada-si          #+#    #+#             */
-/*   Updated: 2024/10/03 10:08:04 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/10/04 09:27:46 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef __PMERGEME_HPP__
-# define __PMERGEME_HPP__
+#ifndef __PMERGEMEV_HPP__
+# define __PMERGEMEV_HPP__
 # include <string>
 # include <iostream>
 # include <iomanip>
 # include <vector>
-# include <algorithm>
-# include <map>
-# define GREEN	"\e[1;32m"
-# define YELLOW	"\e[1;33m"
-# define RED	"\e[1;31m"
-# define BLUE	"\e[1;34m"
-# define PINK	"\e[1;35m"
-# define RESET	"\e[0m"
-
 typedef std::vector<int>	VEC;
 typedef VEC::iterator		ITER;
 typedef VEC::const_iterator	CONST_ITER;
 
-class PmergeMe
+class PmergeMeV
 {
 	private:
 		bool			_odd;
@@ -38,17 +29,19 @@ class PmergeMe
 		VEC				_b;
 		bool			_sorted;
 		unsigned int	_offset;
-		unsigned int	_bSize;
+		VEC				_aIdx;
+		VEC				_bIdx;
 		void			fillPairs();
 		void			sortPairs();
-		void			print() const;
+		void			dichotomy(int value, int valueIdx, float idxRight);
 	public:
-						PmergeMe();
-						PmergeMe(VEC v);
-						PmergeMe(const PmergeMe &src);
-						~PmergeMe();
-		PmergeMe&		operator=(const PmergeMe &src);
+						PmergeMeV();
+						PmergeMeV(VEC v);
+						PmergeMeV(const PmergeMeV &src);
+						~PmergeMeV();
+		PmergeMeV&		operator=(const PmergeMeV &src);
 		VEC				sort();
+		VEC				index();
 };
 
 std::ostream&	operator<<(std::ostream& os, const VEC& v);
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -6,12 +6,25 @@
 /*   By: jeada-si <jeada-si@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/27 11:58:11 by jeada-si          #+#    #+#             */
-/*   Updated: 2024/10/03 10:04:50 by jeada-si         ###   ########.fr       */
+/*   Updated: 2024/10/04 09:58:20 by jeada-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "PmergeMe.hpp"
+#include "PmergeMeV.hpp"
 #include <ctime>
+#include <algorithm>
+
+template<typename T>
+bool	isSorted(T c)
+{
+	for (typename T::iterator it = c.begin(); it != c.end() - 1; it++)
+	{
+		typename T::iterator next = it + 1;
+		if (*it > *next)
+			return false;
+	}
+	return true;
+}
 
 int	main(int ac, char** av)
 {
@@ -21,11 +34,13 @@ int	main(int ac, char** av)
 	VEC	v;
 	while (i < ac)
 		v.push_back(static_cast<int>(std::strtol(av[i++], NULL, 10)));
+	// std::cout << v << std::endl;
 	std::clock_t	t1 = std::clock();
-	PmergeMe	p(v);
-	std::cout << p.sort() << std::endl;
+	PmergeMeV	p(v);
+	p.sort();
+	// std::cout << p.sort() << std::endl;
 	std::clock_t	t2 = clock();
 	std::cout << std::fixed << std::setprecision(4);
 	std::cout << "time is " << static_cast<double>(t2 - t1) / CLOCKS_PER_SEC * 1000 << "ms" << std::endl;
-	
+	std::cout << "is sorted " << (isSorted(p.sort()) == true ? "true": "false") << std::endl;
 }
